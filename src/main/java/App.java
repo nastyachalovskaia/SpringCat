520d95b2a7e73373e6cf10b5dddfea0492b9f5cf
nastyachalovskaia <nastya.chalovskaia@gmail.com>
2022-08-17 10:43:48 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean.equals(catBean2));
+
+        System.out.println(bean.equals(bean2));
+
+    }
+}
